e8754590284b4864e76fd76c55759ec9c98272f4
Chiara Solito <77101645+ChiaraSolito@users.noreply.github.com>
2023-05-13 11:13:25 +0200

    update utils: added aids preprocess
    Added a new dataset in the ones the training script is able to preprocess

--- src/utils.py
+++ src/utils.py
@@ -254,6 +254,43 @@ def preprocess_data(dataset, train_ratio):
         print(dataset, nclass)
 
         return g, nclass, features, labels, train, val, test
+    
+    #experiments on new dataset aids
+    elif dataset in ['aids']:
+        edge = np.loadtxt('./high_freq/{}/edges.txt'.format(dataset), dtype=int)
+        edge = edge[edge[:, 0].argsort()]
+        labels = np.loadtxt('./high_freq/{}/labels.txt'.format(dataset), dtype=int).tolist()
+        features = np.loadtxt('./high_freq/{}/features.txt'.format(dataset), dtype=float)
+
+        U = [e[0]-1 for e in edge]
+        V = [e[1]-1 for e in edge]
+        g = dgl.graph((U, V))
+        g = dgl.to_simple(g)
+        g = dgl.to_bidirected(g)
+        g = dgl.remove_self_loop(g)
+
+        n = len(labels)
+        idx = [i for i in range(n)]
+        #random.shuffle(idx)
+        r0 = int(n * train_ratio)
+        r1 = int(n * 0.6)
+        r2 = int(n * 0.8)
+        train = np.array(idx[:r0])
+        val = np.array(idx[r1:r2])
+        test = np.array(idx[r2:])
+
+        features = normalize_features(features)
+        features = torch.FloatTensor(features)
+
+        nclass = len(set(labels))
+        labels = torch.LongTensor(labels)
+        train = torch.LongTensor(train)
+        val = torch.LongTensor(val)
+        test = torch.LongTensor(test)
+        print(dataset, nclass)
+
+        return g, nclass, features, labels, train, val, test
+
 
 
 def normalize_features(mx):
